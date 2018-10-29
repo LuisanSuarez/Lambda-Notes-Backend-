191e2f2727ab7db548830484fce1f5f4bf9f3ba6
Luisan <luisangelsc@gmail.com>
2018-10-29 12:37:08 -0600

    display a list of notesD

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,12 +3,41 @@ const request = require('supertest');
 const server = require('../api/server.js')
 
 describe('server.js', () => {
-  describe('root endpoint (/)', () => {
+  describe.skip('root endpoint (/)', () => {
 
-    it('should return statuso code 200', async () => {
+    it('should return status code 200', async () => {
       const expected = 200
       const response = await request(server).get('/')
       expect(response.status).toEqual(expected)
+    })
+
+    it('should return "running!" in the body', async () => {
+      const expected = "running!"
+      const response = await request(server).get('/')
+      expect(response.body).toEqual(expected)
+    })
+
+  })
+
+  describe('GET notes (/notes) endpoint', () => {
+
+    it('should run and return status code 200', async () => {
+      const expected = 200
+      const response = await request(server).get('/notes')
+      expect(response.status).toEqual(expected)
+    })
+
+    it.only('should return an array of objects', async () => {
+      const expected = 'object'
+      const response = await request(server).get('/notes')
+      expect(response.body.constructor === Array).toBe(true)
+      expect(typeof response.body[0]).toBe(response.body[0] ? expected : "undefined")
+    })
+
+    it('should return 3 initial notes in the body', async () => {
+      const expected = 3
+      const response = await request(server).get('/notes')
+      expect(response.body.length).toEqual(expected)
     })
 
   })
--- api/server.js
+++ api/server.js
@@ -6,7 +6,18 @@ server.use(express.json())
 const db = require('../data/helpers/Model.js')
 
 server.get('/', (req,res) => {
-  res.status(200).send('running!')
+  res.status(200).json('running!')
 })
+
+server.get('/notes', (req,res) => {
+  db.find()
+    .then(notes => {
+      res.status(200).json(notes)
+    })
+    .catch(err => {
+      res.status(431).json(err)
+    })
+})
+
 
 module.exports = server;
